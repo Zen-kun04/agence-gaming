3651cc47ecb2b4c83eca2cdfcb8f9c62f5aa8ebe
Vincent <fabulet.vincent.simplon@gmail.com>
2023-04-24 15:54:06 +0200

    Form competition

--- src/components/competition.php
+++ src/components/competition.php
@@ -1,3 +1,27 @@
 <?php
     
 ?>
+
+<form action="./index.php" method="POST">
+    <div>
+        <label for="name">Prénom</label>
+        <input type="text" name="name" id="name"/>
+    </div>
+    <div>
+        <label for="description">Description</label>
+        <input type="text" name="description" id="description"/>
+    </div>
+    <div>
+        <label for="city">City</label>
+        <input type="text" name="city" id="city"/>
+    </div>
+    <div>
+        <label for="format">Format</label>
+        <input type="text" name="format" id="format"/>
+    </div>
+    <div>
+        <label for="cash_prize">Cash Prize</label>
+        <input type="text" name="cash_prize" id="cash_prize"/>
+    </div>
+    <input type="submit" value="Envoyer">
+</form>
