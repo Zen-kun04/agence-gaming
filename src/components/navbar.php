d57189c67cfb0fa9d43858e81ce27d250044bf09
Guillaume <estrade.guillaume.simplon@gmail.com>
2023-04-24 16:55:23 +0200

    navbar en cours + css en cours

--- src/components/navbar.php
+++ src/components/navbar.php
@@ -1,3 +1,14 @@
-<?php
-    
-?>
+<header class="header">
+    <a href="#">Accueil</a>
+
+    <nav class="navbar">
+        <ul>
+            <li id="game"><a href="#">Game</a></li>
+            <li id="player"><a href="#">Player</a></li>
+            <li id="team"><a href="#">Team</a></li>
+            <li id="sponsor"><a href="#">Sponsor</a></li>
+            <li id="competition"><a href="#">Compétition</a></li>
+        </ul>
+    </nav>
+</header>
+
