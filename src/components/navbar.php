1b8c45280720cfa3d730c196ab39b1d088866308
Baguette <veryveroniquevery@gmail.com>
2023-04-25 14:31:41 +0200

    DBManager + Players + Games

--- src/components/navbar.php
+++ src/components/navbar.php
@@ -1,14 +1,15 @@
+<link rel="stylesheet" href="../style.css">
 <header class="header">
     <!-- <a href="#">Accueil</a> -->
     <button class="accueilBtn">Accueil</button>
 
     <nav class="navbar">
         <ul>
-            <li id="game"><a href="#">Game</a></li>
-            <li id="player"><a href="#">Player</a></li>
-            <li id="team"><a href="#">Team</a></li>
-            <li id="sponsor"><a href="#">Sponsor</a></li>
-            <li id="competition"><a href="#">Compétition</a></li>
+            <li id="game"><a href="components/game.php">Game</a></li>
+            <li id="player"><a href="components/player.php">Player</a></li>
+            <li id="team"><a href="components/team.php">Team</a></li>
+            <li id="sponsor"><a href="components/sponsor.php">Sponsor</a></li>
+            <li id="competition"><a href="components/competition.php">Compétition</a></li>
         </ul>
     </nav>
 </header>
