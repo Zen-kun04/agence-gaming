d6a2beea996e5495748230228a743da4780fac5c
Baguette <veryveroniquevery@gmail.com>
2023-04-28 11:54:26 +0200

    Fixed DBManager things

---
+++ src/components/managers/DBManager.example.php
@@ -0,0 +1,21 @@
+<?php
+    class DBManager {
+        private $mysql;
+
+        public function __construct() {
+            try {
+                $this->mysql = new PDO(
+                    'mysql:host=localhost;dbname=database;charset=utf8',
+                    'mysql_username',
+                    'mysql_password'
+                );
+            }catch(PDOException $e){
+                die("Error: " .  $e->getMessage());
+            }
+        }
+
+        public function getConnection() {
+            return $this->mysql;
+        }
+    }
+?>
